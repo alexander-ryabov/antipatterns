7a5c4f406c2da461601c38b43a51a83fb73bb4f3
John Jackob <letsdoitagain@tut.by>
2017-03-17 07:48:45 +0300

    first commit

--- src/main/java/suite/longmethod/sample1/App1.java
+++ src/main/java/suite/longmethod/sample1/App1.java
@@ -4,6 +4,16 @@ public class App1 {
 
     public void aLittleBitLongMethod() {
         System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
+        System.out.println(7);
+        System.out.println(8);
+        System.out.println(9);
+        System.out.println(10);
+        System.out.println(1);
         System.out.println(1 + 2);
         System.out.println(1 + 2 + 3);
         System.out.println(1 + 2 + 3 + 4);
@@ -13,7 +23,16 @@ public class App1 {
         System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8);
         System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
         System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
-        System.out.println(1);
+        System.out.println(1*1);
+        System.out.println(2*2);
+        System.out.println(3*3);
+        System.out.println(4*4);
+        System.out.println(5*5);
+        System.out.println(6*6);
+        System.out.println(7*7);
+        System.out.println(8*8);
+        System.out.println(9*9);
+        System.out.println(10*10);
         System.out.println(1 * 2);
         System.out.println(1 * 2 * 3);
         System.out.println(1 * 2 * 3 * 4);
