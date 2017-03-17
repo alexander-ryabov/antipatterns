4407ef640f98cf339a9723901079360e19c4efa2
Alexander Ryabov <alexander.ryabov@gmail.com>
2017-03-17 07:00:07 +0300

    long method candidate

---
+++ src/main/java/suite/longmethod/sample1/App1.java
@@ -0,0 +1,32 @@
+package suite.longmethod.sample1;
+
+public class App1 {
+
+    public void aLittleBitLongMethod() {
+        System.out.println(1);
+        System.out.println(1 + 2);
+        System.out.println(1 + 2 + 3);
+        System.out.println(1 + 2 + 3 + 4);
+        System.out.println(1 + 2 + 3 + 4 + 5);
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6);
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7);
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8);
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+        System.out.println(1);
+        System.out.println(1 * 2);
+        System.out.println(1 * 2 * 3);
+        System.out.println(1 * 2 * 3 * 4);
+        System.out.println(1 * 2 * 3 * 4 * 5);
+        System.out.println(1 * 2 * 3 * 4 * 5 * 6);
+        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7);
+        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8);
+        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9);
+        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+    }
+
+    public static void main(String[] args) {
+        (new App1()).aLittleBitLongMethod();
+    }
+
+}
