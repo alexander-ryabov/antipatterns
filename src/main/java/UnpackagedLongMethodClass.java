e4f937d9cae69c182ccd0ef80d31da3379adddb8
AlexanderRyabov <alexander.ryabov@aurea.com>
2017-04-04 09:14:24 +0300

    3 long method samples

---
+++ src/main/java/UnpackagedLongMethodClass.java
@@ -0,0 +1,40 @@
+public class UnpackagedLongMethodClass {
+
+    public void longMethod() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
+        System.out.println(7);
+        System.out.println(8);
+        System.out.println(9);
+        System.out.println(10);
+        System.out.println(11);
+        System.out.println(12);
+        System.out.println(13);
+        System.out.println(14);
+        System.out.println(15);
+        System.out.println(16);
+        System.out.println(17);
+        System.out.println(18);
+        System.out.println(19);
+        System.out.println(20);
+        System.out.println(21);
+        System.out.println(22);
+        System.out.println(23);
+        System.out.println(24);
+        System.out.println(25);
+        System.out.println(26);
+        System.out.println(27);
+        System.out.println(28);
+        System.out.println(29);
+        System.out.println(30);
+        System.out.println(31);
+        System.out.println(32);
+        System.out.println(33);
+        System.out.println(34);
+    }
+
+}
---
+++ src/main/java/suite/longmethod/sample2/DeepNested.java
@@ -0,0 +1,55 @@
+package suite.longmethod.sample2;
+
+public class DeepNested {
+
+    public static class ClassA {
+        public static class ClassB {
+            public static class ClassC {
+                class ClassD {
+                    public void deepNestedLongMethod() {
+                        System.out.println(1);
+                        System.out.println(2);
+                        System.out.println(3);
+                        System.out.println(4);
+                        System.out.println(5);
+                        System.out.println(6);
+                        System.out.println(7);
+                        System.out.println(8);
+                        System.out.println(9);
+                        System.out.println(10);
+                        System.out.println(1);
+                        System.out.println(1 + 2);
+                        System.out.println(1 + 2 + 3);
+                        System.out.println(1 + 2 + 3 + 4);
+                        System.out.println(1 + 2 + 3 + 4 + 5);
+                        System.out.println(1 + 2 + 3 + 4 + 5 + 6);
+                        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7);
+                        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8);
+                        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+                        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+                        System.out.println(1*1);
+                        System.out.println(2*2);
+                        System.out.println(3*3);
+                        System.out.println(4*4);
+                        System.out.println(5*5);
+                        System.out.println(6*6);
+                        System.out.println(7*7);
+                        System.out.println(8*8);
+                        System.out.println(9*9);
+                        System.out.println(10*10);
+                        System.out.println(1 * 2);
+                        System.out.println(1 * 2 * 3);
+                        System.out.println(1 * 2 * 3 * 4);
+                        System.out.println(1 * 2 * 3 * 4 * 5);
+                        System.out.println(1 * 2 * 3 * 4 * 5 * 6);
+                        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7);
+                        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8);
+                        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9);
+                        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+                    }
+
+                }
+            }
+        }
+    }
+}
